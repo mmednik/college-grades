706600f5ed7b25d1fa991460b404ae53d940b276
mmednik <mmednik@gmail.com>
2021-09-22 20:18:24 -0300

    coding conventions optimizations

--- CollegeGrades.sol
+++ CollegeGrades.sol
@@ -1,61 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
-pragma experimental ABIEncoderV2;
 
-contract grades {
+contract CollegeGrades {
     
     address public teacher;
     
-    constructor () public {
+    constructor() {
         teacher = msg.sender;
     }
-    
     // Associate student with your grade
-    mapping (bytes32 => uint) Grades;
-    
+    mapping(bytes32 => uint) Grades;
     // Grade review requests
-    string [] gradeReviews;
-    
+    string[] gradeReviews;
     // Events
-    event gradedStudent(bytes32, uint);
-    event gradeReview(string);
-    
+    event GradedStudent(bytes32, uint);
+    event GradeReview(string);
     // Grade a student
-    function gradeStudent(string memory _studentId, uint _grade) public TeacherRole(msg.sender) {
-        
+    function gradeStudent(
+        string memory _studentId,
+        uint _grade
+    ) public teacherRole(msg.sender) {
         bytes32 studentIdHash = keccak256(abi.encodePacked(_studentId));
         Grades[studentIdHash] = _grade;
-        emit gradedStudent(studentIdHash, _grade);
-        
+        emit GradedStudent(studentIdHash, _grade);
     }
-    
-    modifier TeacherRole(address _address) {
+
+    modifier teacherRole(address _address) {
         require(_address == teacher, "You do not have privileges to perform this operation.");
         _;
     }
-    
     // Get student grades
-    function getGrade(string memory _studentId) public view returns (uint) {
-        
+    function getGrade(string memory _studentId) public view returns(uint) {
         bytes32 studentIdHash = keccak256(abi.encodePacked(_studentId));
         uint studentGrade = Grades[studentIdHash];
         return studentGrade;
-        
     }
-    
     // Request grade review
     function review(string memory _studentId) public {
-        
         gradeReviews.push(_studentId);
-        emit gradeReview(_studentId);
-        
+        emit GradeReview(_studentId);
     }
-    
     // Get grade reviews 
-    function getReviews() public view TeacherRole(msg.sender) returns (string [] memory) {
-        
+    function getReviews() public view teacherRole(msg.sender) returns(string[] memory) {
         return gradeReviews;    
-        
     }
     
 }
